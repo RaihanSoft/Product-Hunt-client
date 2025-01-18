45de5408e7127e7e684230b6ea58243ba23591e0
Raihan <aboraihan14@gmail.com>
2025-01-18 16:20:30 +0600

    basic setup

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Routes/Routes'
+import { Provider } from './Components/Provider/Provider'
+import { DarkModeProvider } from '../src/Components/Common/DarkMood/DarkMood'
+import { HelmetProvider } from 'react-helmet-async'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HelmetProvider>
+      <Provider>
+        <DarkModeProvider>
+          <RouterProvider router={router} />
+        </DarkModeProvider>
+      </Provider>
+    </HelmetProvider>
+  </StrictMode>,
+)
